7ab48438945b213074567fd51f11fcb6e76580b8
Leandro Calvo <lcalvo.intern@copyright.com>
2022-07-25 08:40:06 +0200

    added template

--- app/components/bookmarks.js
+++ app/components/bookmarks.js
@@ -3,9 +3,9 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class BookmarksComponent extends Component {
-
-    @tracked isLarge = false;
-    @action toggleBookmark() {
-      this.isLarge = !this.isLarge;
-    }
+  @tracked isLarge = false;
+  @action toggleBookmark() {
+    console.log("Hola has pulsado el botón");
+    this.isLarge = !this.isLarge;
+  }
 }
