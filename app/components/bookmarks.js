b43772a369f14fbcddb455e1d66fa18bfb8246bf
Leandro Calvo <lcalvo.intern@copyright.com>
2022-07-26 10:18:07 +0200

    arg ID correctly

--- app/components/bookmarks.js
+++ app/components/bookmarks.js
@@ -9,10 +9,9 @@ export default class BookmarksComponent extends Component {
   //Llamamos al servicio que utilizaremos, mi servicio se llama "bookmarks" 
   @service bookmarks;
   @tracked isGreen = false;
-  @tracked id;
   //El action que me pide el component 
   @action usaServicio() {
-    console.log("Argumento ID: " + this.id);
+    console.log("Argumento ID: " + this.args.id);
     this.isGreen = !this.isGreen;
     console.log("Se ha pulsado el botón");
     
