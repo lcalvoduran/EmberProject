2a0f6b85b662a435c2e3ffeea47e4303a0b9fefc
Leandro Calvo <lcalvo.intern@copyright.com>
2022-07-27 11:00:08 +0200

    a

--- app/services/bookmarks.js
+++ app/services/bookmarks.js
@@ -12,58 +12,15 @@ export default class BookmarksService extends Service {
         /** === Guardado con localStorage ===**/
 
         localStorage.setItem(argID, booleano);
-     
+        localStorage.getItem(argID);
                
     }
 
     loadID(){        
         console.log("=== SERVICES === [loadID]"); 
         console.log("... You're using loadID service ");
-        for (let i = 0; i < localStorage.length; i++) {
-            const key = localStorage.key(i);           
-            console.log("[ID]: " + key + " [STATE]: " + localStorage.getItem(key));
-            if(!key){
-                console.log("No hay datos en localStorage");
-                return false
-            }else{
-                console.log(document.getElementById(key).innerHTML="He cambiado");
-                return true
-            }
-        }
 
-/**
- * 
- * 
- * // get favorites from local storage or empty array
-        var favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-// add class 'fav' to each favorite
-favorites.forEach(function(favorite) {
-  document.getElementById(favorite).className = 'fav';
-});
-// register click event listener
-document.querySelector('.list').addEventListener('click', function(e) {
-  var id = e.target.id,
-      item = e.target,
-      index = favorites.indexOf(id);
-  // return if target doesn't have an id (shouldn't happen)
-  if (!id) return;
-  // item is not favorite
-  if (index == -1) {
-    favorites.push(id);
-    item.className = 'fav';
-  // item is already favorite
-  } else {
-    favorites.splice(index, 1);
-    item.className = '';
-  }
-  // store array in local storage
-  localStorage.setItem('favorites', JSON.stringify(favorites));
-});
 
-// local storage stores strings so we use JSON to stringify for storage and parse to get out of storage
- * 
- * 
- */
 
         
     }
