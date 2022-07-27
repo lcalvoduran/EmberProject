0421203c7c2ed5f9e15bae0e77213458ad87979c
Leandro Calvo <lcalvo.intern@copyright.com>
2022-07-27 11:41:39 +0200

    penultimo goal

--- app/services/bookmarks.js
+++ app/services/bookmarks.js
@@ -1,18 +1,27 @@
 import {A} from '@ember/array';
 import Service from '@ember/service';
 
-export default class BookmarksService extends Service {
+const myObj = [
 
+  ];
+    
+  console.log(myObj);
+
+export default class BookmarksService extends Service {
+   
     //======> 2 Métodos dentro del servicio 
 
     // -> SAVE: Que guarde algo e imprima algo
     saveID(argID, booleano){
         console.log("=== SERVICES === [saveID]"); 
         console.log("... ID: [" + argID + "] [State]: "+ booleano);
-        /** === Guardado con localStorage ===**/
 
+        myObj.push({ID: argID, STATE: booleano});
+        myObj.push();
+        console.log(myObj);
+        //Guardar la lista en el localStorage
         localStorage.setItem(argID, booleano);
-        localStorage.getItem(argID);
+
                
     }
 
