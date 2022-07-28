e1d12ae83b0bca20e394c52d25140c42d7f5b411
Leandro Calvo <lcalvo.intern@copyright.com>
2022-07-28 11:22:40 +0200

    a

--- app/services/bookmarks.js
+++ app/services/bookmarks.js
@@ -7,19 +7,32 @@ export default class BookmarksService extends Service {
 
 //En el servicio tener una lista de objetos {id, estado} y rellenarla con lo que haya en el localStorage en el constructor. 
 //A la hora de guardar añades el nuevo objeto a esa lista, y esa lista es la que escribes en el localStorage
- 
+    BookmarksService(
+        id,
+        state
+    ){
+        this.id = id;
+        this.state = state;
+    }
+
+/**
     myObj;
 
     BookmarksService() {
         this.myObj = this.loadID();
         
     }    
-
+ */
     
     //======> 2 Métodos dentro del servicio 
 
     // -> SAVE: Que guarde algo e imprima algo
-    saveID(argID, booleano){        
+    saveID(argID, booleano){   
+        const nuevoObjeto = new BookmarksService(
+            'hola',
+            'true'
+        );
+        console.log(nuevoObjeto);
         console.log("=== SERVICES === [saveID]"); 
         console.log("... ID: [" + argID + "] [State]: "+ booleano);
         //this.myObj.push({ID: argID, STATE: booleano});
